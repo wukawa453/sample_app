006e3ccf2e913d6d73c16359aae26617a3d5ef64
Christopher Wu <chriswu452@gmail.com>
2015-02-04 17:28:05 -0800

    Added Contact Page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -21,6 +21,12 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | #{@base_title}" 
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+  get :contact
+  assert_response :success
+  assert_select "title", "Contact | #{@base_title}"
   end
 end
